6db17c01910d2cc9999d676f044287b92a9cf03f
Kenneth Kiang <kiangjiaming@gmail.com>
2024-09-22 03:29:07 +0800

    set up operator boilerplate

---
+++ api/v1/podmigration_types.go
@@ -0,0 +1,86 @@
+/*
+Copyright 2024.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
+// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+
+// PodMigrationSpec defines the desired state of PodMigration
+type PodMigrationSpec struct {
+	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+
+	// +kubebuilder:validation:MinLength=0
+	PodName string `json:"podName"`
+
+	// +kubebuilder:validation:MinLength=0
+	// +optional
+	NodeName string `json:"nodeName"`
+
+	// +kubebuilder:validation:MinLength=0
+	// +optional
+	NodeSelector string `json:"nodeSelector"`
+}
+
+type MigrationStatus string
+
+const (
+	Pending   MigrationStatus = "Pending"
+	Running   MigrationStatus = "Running"
+	Succeeded MigrationStatus = "Succeeded"
+	Failed    MigrationStatus = "Failed"
+)
+
+// PodMigrationStatus defines the observed state of PodMigration
+type PodMigrationStatus struct {
+	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+
+	Status MigrationStatus `json:"status"`
+
+	// Information when was the migration completed
+	LastCompletedTime *metav1.Time `json:"lastScheduleTime,omitempty"`
+}
+
+// +kubebuilder:object:root=true
+// +kubebuilder:subresource:status
+
+// PodMigration is the Schema for the podmigrations API
+type PodMigration struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   PodMigrationSpec   `json:"spec,omitempty"`
+	Status PodMigrationStatus `json:"status,omitempty"`
+}
+
+// +kubebuilder:object:root=true
+
+// PodMigrationList contains a list of PodMigration
+type PodMigrationList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []PodMigration `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&PodMigration{}, &PodMigrationList{})
+}
